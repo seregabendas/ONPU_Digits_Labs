66e6622465b9b55cad8d6659f99f0e672318b92c
Sergey Bendas <seregabendas@gmail.com>
2018-03-16 19:48:04 +0200

    Lab1, Class Bitmap

---
+++ lab_1/Laba_1/Laba_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Laba_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap bitmapBefore = new Bitmap(@"C:\Users\sereg\Desktop\ONPU_Digits\img\ris.bmp");
+            Bitmap bitmapResult = new Bitmap(bitmapBefore.Width, bitmapBefore.Height);
+
+            for (int i = 0; i < bitmapBefore.Width; i++)
+            {
+                for (int j = 0; j < bitmapBefore.Height; j++)
+                {
+                    Color colorBefore = bitmapBefore.GetPixel(i, j);
+                    byte redColor = colorBefore.R;
+                    Color colorResult = Color.FromArgb(redColor, 0, 0);
+                    bitmapResult.SetPixel(i, j, colorResult);
+                }
+            }
+
+            bitmapResult.Save(@"C:\Users\sereg\Desktop\ONPU_Digits\img\ris2.bmp");
+
+            
+
+
+        }
+    }
+}
