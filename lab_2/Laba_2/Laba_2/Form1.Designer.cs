b8da545f703bfd2782e83cbebce890d969da9fe3
Sergey Bendas <seregabendas@gmail.com>
2018-03-17 10:51:17 +0200

    Laba 2

---
+++ lab_2/Laba_2/Laba_2/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Laba_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbSource = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pbResult = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnGrey = new System.Windows.Forms.Button();
+            this.btnNegative = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbSource
+            // 
+            this.pbSource.Location = new System.Drawing.Point(12, 48);
+            this.pbSource.Name = "pbSource";
+            this.pbSource.Size = new System.Drawing.Size(580, 350);
+            this.pbSource.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSource.TabIndex = 0;
+            this.pbSource.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(18, 18);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // pbResult
+            // 
+            this.pbResult.Location = new System.Drawing.Point(615, 48);
+            this.pbResult.Name = "pbResult";
+            this.pbResult.Size = new System.Drawing.Size(580, 350);
+            this.pbResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbResult.TabIndex = 2;
+            this.pbResult.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // btnGrey
+            // 
+            this.btnGrey.Location = new System.Drawing.Point(93, 12);
+            this.btnGrey.Name = "btnGrey";
+            this.btnGrey.Size = new System.Drawing.Size(75, 23);
+            this.btnGrey.TabIndex = 4;
+            this.btnGrey.Text = "Grey";
+            this.btnGrey.UseVisualStyleBackColor = true;
+            this.btnGrey.Click += new System.EventHandler(this.btnGrey_Click);
+            // 
+            // btnNegative
+            // 
+            this.btnNegative.Location = new System.Drawing.Point(174, 12);
+            this.btnNegative.Name = "btnNegative";
+            this.btnNegative.Size = new System.Drawing.Size(75, 23);
+            this.btnNegative.TabIndex = 5;
+            this.btnNegative.Text = "Negativ";
+            this.btnNegative.UseVisualStyleBackColor = true;
+            this.btnNegative.Click += new System.EventHandler(this.btnNegative_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1207, 544);
+            this.Controls.Add(this.btnNegative);
+            this.Controls.Add(this.btnGrey);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.pbResult);
+            this.Controls.Add(this.pbSource);
+            this.Name = "Form1";
+            this.Text = "Laba 2";
+            ((System.ComponentModel.ISupportInitialize)(this.pbSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbSource;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox pbResult;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnGrey;
+        private System.Windows.Forms.Button btnNegative;
+    }
+}
+
---
+++ lab_2/Laba_2/Laba_2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_2
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap _pixelMatrix;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+
+            if (openFileDialog1.FileName.Equals("openFileDialog1"))
+            {
+                return;
+            }
+            _pixelMatrix = new Bitmap(openFileDialog1.FileName);
+            pbSource.Image = _pixelMatrix;
+        }
+
+        private void btnGrey_Click(object sender, EventArgs e)
+        {
+            if (_pixelMatrix == null)
+            {
+                return;
+            }
+
+            var pixelMatrixResult = new Bitmap(_pixelMatrix.Width, _pixelMatrix.Height);
+            for (var i = 0; i < _pixelMatrix.Width; i++)
+            {
+                for (var j = 0; j < _pixelMatrix.Height; j++)
+                {
+                    var color = _pixelMatrix.GetPixel(i, j);
+                    var red = color.R;
+                    var blue = color.B;
+                    var green = color.G;
+                    var grey = (77 * red + 150 * green + 29 * blue) / 256;
+
+                    var newColor = Color.FromArgb(grey, grey, grey);
+                    pixelMatrixResult.SetPixel(i, j, newColor);
+                }
+            }
+
+            pbResult.Image = pixelMatrixResult;
+        }
+
+        private void btnNegative_Click(object sender, EventArgs e)
+        {
+            if (_pixelMatrix == null)
+            {
+                return;
+            }
+
+            var pixelMatrixResult = new Bitmap(_pixelMatrix.Width, _pixelMatrix.Height);
+            for (var i = 0; i < _pixelMatrix.Width; i++)
+            {
+                for (var j = 0; j < _pixelMatrix.Height; j++)
+                {
+                    var color = _pixelMatrix.GetPixel(i, j);
+                    var newRed = 255 - color.R;
+                    var newBlue = 255 -color.B;
+                    var newGreen = 255 - color.G;
+
+                    var newColor = Color.FromArgb(newRed, newGreen, newBlue);
+                    pixelMatrixResult.SetPixel(i, j, newColor);
+                }
+            }
+
+            pbResult.Image = pixelMatrixResult;
+        }
+    }
+}
