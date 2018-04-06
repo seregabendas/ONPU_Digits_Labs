b8fcbbb79dc7431dd827370d0a449c30d007faa1
Sergey Bendas <seregabendas@gmail.com>
2018-04-06 11:21:28 +0300

    Laba 4

---
+++ lab5/Laba5/Laba5/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Laba5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbSource = new System.Windows.Forms.PictureBox();
+            this.pbResult = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnRotate = new System.Windows.Forms.Button();
+            this.tbRotate = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbRotate)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbSource
+            // 
+            this.pbSource.Location = new System.Drawing.Point(12, 12);
+            this.pbSource.Name = "pbSource";
+            this.pbSource.Size = new System.Drawing.Size(544, 326);
+            this.pbSource.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSource.TabIndex = 0;
+            this.pbSource.TabStop = false;
+            // 
+            // pbResult
+            // 
+            this.pbResult.Location = new System.Drawing.Point(605, 12);
+            this.pbResult.Name = "pbResult";
+            this.pbResult.Size = new System.Drawing.Size(544, 326);
+            this.pbResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbResult.TabIndex = 1;
+            this.pbResult.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(13, 357);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnRotate
+            // 
+            this.btnRotate.Location = new System.Drawing.Point(13, 387);
+            this.btnRotate.Name = "btnRotate";
+            this.btnRotate.Size = new System.Drawing.Size(75, 23);
+            this.btnRotate.TabIndex = 3;
+            this.btnRotate.Text = "Rotate";
+            this.btnRotate.UseVisualStyleBackColor = true;
+            this.btnRotate.Click += new System.EventHandler(this.btnRotate_Click);
+            // 
+            // tbRotate
+            // 
+            this.tbRotate.Location = new System.Drawing.Point(121, 359);
+            this.tbRotate.Maximum = 360;
+            this.tbRotate.Name = "tbRotate";
+            this.tbRotate.Size = new System.Drawing.Size(104, 50);
+            this.tbRotate.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1161, 449);
+            this.Controls.Add(this.tbRotate);
+            this.Controls.Add(this.btnRotate);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.pbResult);
+            this.Controls.Add(this.pbSource);
+            this.Name = "Form1";
+            this.Text = "pix";
+            ((System.ComponentModel.ISupportInitialize)(this.pbSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbRotate)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbSource;
+        private System.Windows.Forms.PictureBox pbResult;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnRotate;
+        private System.Windows.Forms.TrackBar tbRotate;
+    }
+}
+
---
+++ lab5/Laba5/Laba5/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba5
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap _pixelMatrix;
+        private Bitmap _pixelMatrixResult;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+
+            if (openFileDialog1.FileName.Equals(""))
+            {
+                return;
+            }
+
+            _pixelMatrix = new Bitmap(openFileDialog1.FileName);
+            pbSource.Image = _pixelMatrix;
+        }
+
+        private void btnRotate_Click(object sender, EventArgs e)
+        {
+            if (_pixelMatrix == null)
+            {
+                return;
+            }
+
+            _pixelMatrixResult = new Bitmap(_pixelMatrix.Width, _pixelMatrix.Height);
+            var grad = tbRotate.Value;
+            var rad = grad * Math.PI / 180;
+            var xc = _pixelMatrix.Width / 2;
+            var yc = _pixelMatrix.Height / 2;
+            var radCos = Math.Cos(rad);
+            var radSin = Math.Sin(rad);
+            for (var i = 0; i < _pixelMatrix.Width; i++)
+            {
+                for (var j = 0; j < _pixelMatrix.Height; j++)
+                {
+                    var ix = i - xc;
+                    var jy = j - yc;
+                    var x = (int) (ix * radCos - jy * radSin + xc);
+                    var y = (int) (ix * radSin + jy * radCos + yc);
+                    if (x >= 0 && x < _pixelMatrix.Width && y >= 0 && y < _pixelMatrix.Height)
+                    {
+                        var col = _pixelMatrix.GetPixel(x, y);
+                        _pixelMatrixResult.SetPixel(i, j, col);
+                    }
+                }
+            }
+
+            pbResult.Image = _pixelMatrixResult;
+        }
+    }
+}
