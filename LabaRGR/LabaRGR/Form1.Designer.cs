196b9d2a0db98cd3ba715dd03962a5d704a6b5a5
Sergey Bendas <seregabendas@gmail.com>
2018-04-27 09:19:35 +0300

    RGR

--- LabaRGR/LabaRGR/Form1.Designer.cs
+++ LabaRGR/LabaRGR/Form1.Designer.cs
@@ -44,6 +44,13 @@
             this.chartRed = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chartBlue = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chartGreen = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tbRmin = new System.Windows.Forms.TextBox();
+            this.tbRmax = new System.Windows.Forms.TextBox();
+            this.tbGmin = new System.Windows.Forms.TextBox();
+            this.tbGmax = new System.Windows.Forms.TextBox();
+            this.tbBmin = new System.Windows.Forms.TextBox();
+            this.tbBmax = new System.Windows.Forms.TextBox();
+            this.btnHist = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbResult)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartRed)).BeginInit();
@@ -83,6 +90,7 @@
             // 
             chartArea1.Name = "ChartArea1";
             this.chartRed.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
             legend1.Name = "Legend1";
             this.chartRed.Legends.Add(legend1);
             this.chartRed.Location = new System.Drawing.Point(133, 361);
@@ -96,12 +104,13 @@
             this.chartRed.Series.Add(series1);
             this.chartRed.Size = new System.Drawing.Size(370, 220);
             this.chartRed.TabIndex = 3;
-            this.chartRed.Text = "Red";
+            this.chartRed.Click += new System.EventHandler(this.chartRed_Click);
             // 
             // chartBlue
             // 
             chartArea2.Name = "ChartArea1";
             this.chartBlue.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
             legend2.Name = "Legend1";
             this.chartBlue.Legends.Add(legend2);
             this.chartBlue.Location = new System.Drawing.Point(885, 360);
@@ -118,6 +127,7 @@
             // 
             chartArea3.Name = "ChartArea1";
             this.chartGreen.ChartAreas.Add(chartArea3);
+            legend3.Enabled = false;
             legend3.Name = "Legend1";
             this.chartGreen.Legends.Add(legend3);
             this.chartGreen.Location = new System.Drawing.Point(509, 361);
@@ -133,11 +143,70 @@
             this.chartGreen.TabIndex = 5;
             this.chartGreen.Text = "Green";
             // 
+            // tbRmin
+            // 
+            this.tbRmin.Location = new System.Drawing.Point(133, 598);
+            this.tbRmin.Name = "tbRmin";
+            this.tbRmin.Size = new System.Drawing.Size(100, 20);
+            this.tbRmin.TabIndex = 6;
+            // 
+            // tbRmax
+            // 
+            this.tbRmax.Location = new System.Drawing.Point(240, 598);
+            this.tbRmax.Name = "tbRmax";
+            this.tbRmax.Size = new System.Drawing.Size(100, 20);
+            this.tbRmax.TabIndex = 7;
+            // 
+            // tbGmin
+            // 
+            this.tbGmin.Location = new System.Drawing.Point(509, 598);
+            this.tbGmin.Name = "tbGmin";
+            this.tbGmin.Size = new System.Drawing.Size(100, 20);
+            this.tbGmin.TabIndex = 8;
+            // 
+            // tbGmax
+            // 
+            this.tbGmax.Location = new System.Drawing.Point(616, 598);
+            this.tbGmax.Name = "tbGmax";
+            this.tbGmax.Size = new System.Drawing.Size(100, 20);
+            this.tbGmax.TabIndex = 9;
+            // 
+            // tbBmin
+            // 
+            this.tbBmin.Location = new System.Drawing.Point(885, 598);
+            this.tbBmin.Name = "tbBmin";
+            this.tbBmin.Size = new System.Drawing.Size(100, 20);
+            this.tbBmin.TabIndex = 10;
+            // 
+            // tbBmax
+            // 
+            this.tbBmax.Location = new System.Drawing.Point(992, 597);
+            this.tbBmax.Name = "tbBmax";
+            this.tbBmax.Size = new System.Drawing.Size(100, 20);
+            this.tbBmax.TabIndex = 11;
+            // 
+            // btnHist
+            // 
+            this.btnHist.Location = new System.Drawing.Point(13, 597);
+            this.btnHist.Name = "btnHist";
+            this.btnHist.Size = new System.Drawing.Size(75, 23);
+            this.btnHist.TabIndex = 13;
+            this.btnHist.Text = "HistChange";
+            this.btnHist.UseVisualStyleBackColor = true;
+            this.btnHist.Click += new System.EventHandler(this.btnHist_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1275, 738);
+            this.ClientSize = new System.Drawing.Size(1275, 633);
+            this.Controls.Add(this.btnHist);
+            this.Controls.Add(this.tbBmax);
+            this.Controls.Add(this.tbBmin);
+            this.Controls.Add(this.tbGmax);
+            this.Controls.Add(this.tbGmin);
+            this.Controls.Add(this.tbRmax);
+            this.Controls.Add(this.tbRmin);
             this.Controls.Add(this.chartGreen);
             this.Controls.Add(this.chartBlue);
             this.Controls.Add(this.chartRed);
@@ -152,6 +221,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.chartBlue)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartGreen)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -164,6 +234,13 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chartRed;
         private System.Windows.Forms.DataVisualization.Charting.Chart chartBlue;
         private System.Windows.Forms.DataVisualization.Charting.Chart chartGreen;
+        private System.Windows.Forms.TextBox tbRmin;
+        private System.Windows.Forms.TextBox tbRmax;
+        private System.Windows.Forms.TextBox tbGmin;
+        private System.Windows.Forms.TextBox tbGmax;
+        private System.Windows.Forms.TextBox tbBmin;
+        private System.Windows.Forms.TextBox tbBmax;
+        private System.Windows.Forms.Button btnHist;
     }
 }
 
--- LabaRGR/LabaRGR/Form1.cs
+++ LabaRGR/LabaRGR/Form1.cs
@@ -12,7 +12,9 @@ namespace LabaRGR
 {
     public partial class Form1 : Form
     {
+        // Обьявление глобальной переменной исходного изорбражения
         private Bitmap _pixelMatrix;
+        // Обьявление глобальной переменной результирующего изображения 
         private Bitmap _pixelMatrixResult;
 
         public Form1()
@@ -20,51 +22,154 @@ namespace LabaRGR
             InitializeComponent();
         }
 
+        // Обработчик кнопки открытия файла
         private void btnOpen_Click(object sender, EventArgs e)
         {
+            // Открытие диалогового окна выбора изображения
             openFileDialog1.ShowDialog();
-
-            if (openFileDialog1.FileName.Equals(""))
-            {
-                return;
-            }
-
+            // Возврат из обработчика при отсутствии названия файла
+            if (openFileDialog1.FileName.Equals("")) return;
+            // Создание объекта класса Bitmap c использованием 
+            // контструктора с параметром пути к файлу и
+            // присваиваение обьекта ссылке _pixelMatrix
             _pixelMatrix = new Bitmap(openFileDialog1.FileName);
+            // Отображение изображения в елементе pbSource
             pbSource.Image = _pixelMatrix;
+            // Создание двумерного массива для зачений гистограмм
+            // получаемых в результате выполнения функции
+            // getHistogram с входным параметром объектом Bitmap
+            // исходного изображения
             int[][] arrays = getHistogram(_pixelMatrix);
+            // Вызов функции отображения гистограммы с передачей 
+            // трех параметров данных о гистограмме каждого цвета
             showHistogram(arrays[0], arrays[1], arrays[2]);
         }
 
+
+        // Функция получения двумерного массива значений
+        // гистограмм каждого канала изображения
         private int[][] getHistogram(Bitmap bitmap)
         {
+            // Создание массивов длинной 256 для каждого цвета,
+            // для хранения повторений каждого значения цвета
+            // где индекс ибозначает значение цвета 
             int[] arrayRed = new int[256];
             int[] arrayBlue = new int[256];
             int[] arrayGreen = new int[256];
+            // Цикл для посещение каждого столбца изображения
             for (int i = 0; i < bitmap.Width; i++)
             {
+                //Цикл для посещения кажой строки изображения
                 for (int j = 0; j < bitmap.Height; j++)
                 {
+                    // Создание переменной цвета и присваивание
+                    // ей значение пикселя координат i, j
                     Color color = bitmap.GetPixel(i, j);
+                    // Определение значение цвета каждого 
+                    // расчет количества повторений
                     arrayRed[color.R]++;
                     arrayBlue[color.B]++;
                     arrayGreen[color.G]++;
                 }
             }
+            // Возврат двумерного массива, со значениями каждого 
+            // массива каждого цвета
             return new int[][] { arrayRed, arrayGreen, arrayBlue };
         }
 
+        // Функция отображения гистограмм каждого цветового канала
         private void showHistogram(int[] arrayRed, int[] arrayGreen, int[] arrayBlue)
         {
+            // Очистка текущих значений графиков
             chartRed.Series["Red"].Points.Clear();
             chartGreen.Series["Green"].Points.Clear();
             chartBlue.Series["Blue"].Points.Clear();
-
+            // Цикл для посещения каждого елемента 
+            // массивов значений цветов и добавления
+            // значения оси Y по оси X графиков
             for (int i = 0; i < 256; i++)
             {
+                // Определение значений массива и добавление 
+                // этого значения на график
                 chartRed.Series["Red"].Points.AddY(arrayRed[i]);
                 chartGreen.Series["Green"].Points.AddY(arrayGreen[i]);
                 chartBlue.Series["Blue"].Points.AddY(arrayBlue[i]);
             }
+        }
+
+        private void chartRed_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Обработчик кнопки гистограммной обработки изображений
+        private void btnHist_Click(object sender, EventArgs e)
+        {
+            // Возврат из обработчика при отсутствии изображения
+            if (_pixelMatrix == null) return;
+            // Создание результирующего изображения с размерами исходного
+            _pixelMatrixResult = new Bitmap(_pixelMatrix.Width, _pixelMatrix.Height);
+            // Создание переменных минимальных и максимальных
+            // значений каждого цветового канала с присваиванием
+            // крайних значений
+            int Rmin = 0;
+            int Rmax = 255;
+            int Gmin = 0;
+            int Gmax = 255;
+            int Bmin = 0;
+            int Bmax = 255;
+            // Присваивание переменным минмальных и максимальных значений 
+            // значений из соответствующих элементов textbox преобразованныых
+            // в тип int c предварительной проверкой на присутствие значений
+            // в соответствующих элементах texbox
+            if (!tbRmin.Text.Equals("")) Rmin = Convert.ToInt32(tbRmin.Text);
+            if (!tbRmax.Text.Equals("")) Rmax = Convert.ToInt32(tbRmax.Text);
+            if (!tbGmin.Text.Equals("")) Gmin = Convert.ToInt32(tbGmin.Text);
+            if (!tbGmax.Text.Equals("")) Gmax = Convert.ToInt32(tbGmax.Text);
+            if (!tbBmin.Text.Equals("")) Bmin = Convert.ToInt32(tbBmin.Text);
+            if (!tbBmax.Text.Equals("")) Bmax = Convert.ToInt32(tbBmax.Text);
+            // Цикл для посещение каждого столбца изображения
+            for (int i = 0; i < _pixelMatrix.Width; i++)
+            {
+                //Цикл для посещения кажой строки изображения
+                for (int j = 0; j < _pixelMatrix.Height; j++)
+                {
+                    // Создание переменной цвета и присваивание
+                    // ей значение пикселя координат i, j
+                    Color col = _pixelMatrix.GetPixel(i, j);
+                    // Создание переменной значений каждого
+                    // цветового канала
+                    int oldR = col.R;
+                    int oldG = col.G;
+                    int oldB = col.B;
+                    // Расчет новых значений каждого цветового канала
+                    int newR = (int)((oldR - Rmin) * (255.0 / (Rmax - Rmin)));
+                    int newG = (int)((oldG - Gmin) * (255.0 / (Gmax - Gmin)));
+                    int newB = (int)((oldB - Bmin) * (255.0 / (Bmax - Bmin)));
+                    // Проверка новых значений на минимальное и 
+                    // максимальное значение
+                    if (newR < 0) newR = 0;
+                    if (newR > 255) newR = 255;
+                    if (newG < 0) newG = 0;
+                    if (newG > 255) newG = 255;
+                    if (newB < 0) newB = 0;
+                    if (newB > 255) newB = 255;
+                    // Присваивание пикслелю с координатами i,j результирующего
+                    // изображения нового цвета
+                    _pixelMatrixResult.SetPixel(i, j, Color.FromArgb(newR, newG, newB));
+                }        
+            }
+            // Отображение нового изображения в елементе
+            // pictureBox результирующего изображения
+            pbResult.Image = _pixelMatrixResult;
+            // Создание двумерного массива для зачений гистограмм
+            // получаемых в результате выполнения функции
+            // getHistogram с входным параметром объектом Bitmap
+            // результирующего изображения
+            int[][] arrays = getHistogram(_pixelMatrixResult);
+            // Вызов функции отображения гистограммы с передачей 
+            // трех параметров данных о гистограмме каждого цвета
+            showHistogram(arrays[0], arrays[1], arrays[2]);
         }
     }
 }
