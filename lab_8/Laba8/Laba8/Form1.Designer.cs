33e55ba8afa2a0938f73f3d380e0fe4134918500
Sergey Bendas <seregabendas@gmail.com>
2018-04-27 09:19:51 +0300

    Laba 8

---
+++ lab_8/Laba8/Laba8/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Laba8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pbSource = new System.Windows.Forms.PictureBox();
+            this.pbResult = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnMedianFilter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbSource
+            // 
+            this.pbSource.Location = new System.Drawing.Point(13, 13);
+            this.pbSource.Name = "pbSource";
+            this.pbSource.Size = new System.Drawing.Size(517, 312);
+            this.pbSource.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSource.TabIndex = 0;
+            this.pbSource.TabStop = false;
+            // 
+            // pbResult
+            // 
+            this.pbResult.Location = new System.Drawing.Point(549, 13);
+            this.pbResult.Name = "pbResult";
+            this.pbResult.Size = new System.Drawing.Size(517, 312);
+            this.pbResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbResult.TabIndex = 1;
+            this.pbResult.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(13, 332);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnMedianFilter
+            // 
+            this.btnMedianFilter.Location = new System.Drawing.Point(95, 332);
+            this.btnMedianFilter.Name = "btnMedianFilter";
+            this.btnMedianFilter.Size = new System.Drawing.Size(75, 23);
+            this.btnMedianFilter.TabIndex = 3;
+            this.btnMedianFilter.Text = "MedFilter";
+            this.btnMedianFilter.UseVisualStyleBackColor = true;
+            this.btnMedianFilter.Click += new System.EventHandler(this.btnMedianFilter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 524);
+            this.Controls.Add(this.btnMedianFilter);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.pbResult);
+            this.Controls.Add(this.pbSource);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pbSource;
+        private System.Windows.Forms.PictureBox pbResult;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnMedianFilter;
+    }
+}
+
---
+++ lab_8/Laba8/Laba8/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba8
+{
+    public partial class Form1 : Form
+    {
+
+        private Bitmap _pixelMatrix;
+        private Bitmap _pixelMatrixResult;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName.Equals("")) return;
+            _pixelMatrix = new Bitmap(openFileDialog1.FileName);
+            pbSource.Image = _pixelMatrix;
+        }
+
+        private void btnMedianFilter_Click(object sender, EventArgs e)
+        {
+            if (_pixelMatrix == null) return;
+            _pixelMatrixResult = new Bitmap(_pixelMatrix.Width, _pixelMatrix.Height);
+
+            int[] arrR = new int[9];
+            int[] arrG = new int[9];
+            int[] arrB = new int[9];
+            for (int i = 1; i < _pixelMatrix.Width - 1; i++)
+            {
+                for (int j = 1; j < _pixelMatrix.Height - 1; j++)
+                {
+                    int n = 0;
+                    for (int k = -1; k <= 1; k++)
+                    {
+                        for (int p = -1; p <= 1; p++)
+                        {
+                            Color col = _pixelMatrix.GetPixel(i + k, j + p);
+                            arrR[n] = col.R;
+                            arrG[n] = col.G;
+                            arrB[n] = col.B;
+                            n++;
+                        }
+                    }
+                    Array.Sort(arrR);
+                    Array.Sort(arrG);
+                    Array.Sort(arrB);
+                    _pixelMatrixResult.SetPixel(i, j, Color.FromArgb(arrR[4], arrG[4], arrB[4]));
+                }
+            }
+            pbResult.Image = _pixelMatrixResult;
+            
+        }
+    }
+}
